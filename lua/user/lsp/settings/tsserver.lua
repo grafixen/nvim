091af37c364dbbc91b6126fa6e1d03e39ba4a716
David Chau <david.chau@egplusww.com>
2022-02-09 23:45:01 +0800

    additional ls config: emmet_ls & tsserver

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,25 @@
+-- https://github.com/jose-elias-alvarez/nvim-lsp-ts-utils
+
+local buf_map = function(bufnr, mode, lhs, rhs, opts)
+  vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts or {
+    noremap = true, silent = true,
+  })
+end
+
+return {
+  init_options = require("nvim-lsp-ts-utils").init_options,
+  --
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+
+    local ts_utils = require("nvim-lsp-ts-utils")
+    ts_utils.setup({})
+    ts_utils.setup_client(client)
+
+    buf_map(bufnr, "n", "gs", ":TSLspOrganize<CR>")
+    buf_map(bufnr, "n", "gi", ":TSLspRenameFile<CR>")
+    buf_map(bufnr, "n", "go", ":TSLspImportAll<CR>")
+  end,
+}
+
