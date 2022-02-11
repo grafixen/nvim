d73ce7b120702fa6d0870aa1e9710426ace048b8
Pui-Ching Chau <chaupc@yahoo.com>
2022-02-11 22:17:51 +0800

    chore: code formatting

--- init.lua
+++ init.lua
@@ -1,21 +1,25 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.bufferline"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
-require "user.whichkey"
-require "user.autocommands"
+require("user.alpha")
+require("user.autocommands")
+require("user.autopairs")
+require("user.bufferline")
+require("user.cmp")
+require("user.coc")
+require("user.colorscheme")
+require("user.comment")
+require("user.gitsigns")
+require("user.hop")
+require("user.impatient")
+require("user.indentline")
+require("user.keymaps")
+require("user.lualine")
+require("user.lsp")
+require("user.move")
+require("user.nvim-tree")
+require("user.options")
+require("user.plugins")
+require("user.project")
+require("user.rnvimr")
+require("user.telescope")
+require("user.toggleterm")
+require("user.treesitter")
+require("user.whichkey")
