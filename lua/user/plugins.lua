bf56d6a273726b8c50b4745e7d7604c034cfcd2d
Pui-Ching Chau <chaupc@yahoo.com>
2022-03-09 22:45:06 +0800

    use ultisnips instead of luasnips

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -102,8 +102,12 @@ return packer.startup(function(use)
   use("hrsh7th/cmp-emoji")
 
 	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+	-- use("L3MON4D3/LuaSnip") --snippet engine
+	-- use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+
+  use('SirVer/ultisnips')
+  use('quangnguyen30192/cmp-nvim-ultisnips')
+  use('honza/vim-snippets')
 
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
---
+++ lua/user/ultisnips.lua
@@ -0,0 +1,5 @@
+vim.g.UltiSnipsExpandTrigger       = '<c-j>'
+vim.g.UltiSnipsListSnippets        = '<c-l>'
+vim.g.UltiSnipsJumpForwardTrigger  = '<Tab>'
+vim.g.UltiSnipsJumpBackwardTrigger = '<S-Tab>'
+
