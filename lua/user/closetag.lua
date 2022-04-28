271b02a1590caa3fce48a69c5671f43e081415e6
Pui-Ching Chau <chaupc@yahoo.com>
2022-04-28 16:00:45 +0800

    new plugin: close tag

---
+++ lua/user/closetag.lua
@@ -0,0 +1,31 @@
+-- filenames like *.xml, *.html, *.xhtml, ...
+-- These are the file extensions where this plugin is enabled.
+vim.g.closetag_filenames = '*.html,*.xhtml,*.phtml,*.ftl'
+
+-- filenames like *.xml, *.xhtml, ...
+-- This will make the list of non-closing tags self-closing in the specified files.
+vim.g.closetag_xhtml_filenames = '*.xhtml,*.jsx'
+
+-- filetypes like xml, html, xhtml, ...
+-- These are the file types where this plugin is enabled.
+vim.g.closetag_filetypes = 'html,xhtml,phtml,ftl'
+
+-- filetypes like xml, xhtml, ...
+vim.g.closetag_xhtml_filetypes = 'xhtml,jsx'
+
+-- integer value [0|1]
+vim.g.closetag_emptyTags_caseSensitive = 1
+
+-- dict
+vim.g.closetag_regions = {
+  ['typescript.tsx'] = 'jsxRegion,tsxRegion',
+  ['javascript.jsx'] = 'jsxRegion',
+  ['typescriptreact'] = 'jsxRegion,tsxRegion',
+  ['javascriptreact'] = 'jsxRegion',
+}
+
+-- Shortcut for closing tags, default is '>'
+vim.g.closetag_shortcut = '>'
+
+-- Add > at current position without closing the current tag, default is ''
+vim.g.closetag_close_shortcut = '<leader>>'
