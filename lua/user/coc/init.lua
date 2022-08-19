4bdd2e5cf63e195701c7f1436682bc738d660400
Pui-Ching Chau <grafixen@gmail.com>
2022-08-19 23:00:05 +0800

    removed coc

--- lua/user/coc/init.lua
+++
@@ -1,5 +0,0 @@
--- Prettier
-vim.cmd([[ command! CocPrInit execute 'CocCommand prettier.createConfigFile' ]])
-vim.cmd([[ command! CocPrOutput execute 'CocCommand prettier.openOutput' ]])
-vim.cmd([[ command! CocPrForce execute 'CocCommand prettier.forceFormatDocument' ]])
-vim.cmd([[ command! CocPrettier execute 'CocCommand prettier.formatFile' ]])
