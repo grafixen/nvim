5fd4a7524b991957b939fcfb25f3542006b7bc09
Pui-Ching Chau <grafixen@gmail.com>
2022-08-20 17:32:05 +0800

    Get Capabilities

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -159,6 +159,7 @@ local mappings = {
 	l = {
 		name = "LSP",
 		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+		c = { "<cmd>lua require('user.lsp').server_capabilities()<cr>", "Get Capabilities" },
 		D = {
 			"<cmd>Telescope diagnostics<cr>",
 			"Document Diagnostics",
@@ -171,7 +172,7 @@ local mappings = {
 		F = { "<cmd>lua vim.lsp.buf.range_formatting()<cr>", "Range Format" },
 		i = { "<cmd>LspInfo<cr>", "LSP Info" },
 		I = { "<cmd>NullLsInfo<cr>", "Null-ls Info" },
-		c = { "<cmd>LspRename<CR>", "Rename" },
+		rn = { "<cmd>LspRename<CR>", "Rename" },
 		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
 		S = {
 			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
@@ -185,8 +186,8 @@ local mappings = {
 			"<cmd>LspDiagPrev<CR>",
 			"Prev Diagnostic",
 		},
-		Q = { "<cmd>Telescope lsp_references<CR>", "References" },
 		q = { "<cmd>LspQuickfix<CR>", "Quickfix" },
+		r = { "<cmd>Telescope lsp_references<CR>", "References" },
 	},
 	r = {
 		name = "Replace",
