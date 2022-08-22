57d7fe5be67d5e575750ae1e39dc69a6a5f73bbe
Pui-Ching Chau <grafixen@gmail.com>
2022-08-22 09:11:58 +0800

    housekeeping

--- lua/user/nerdcommenter.lua
+++ lua/user/nerdcommenter.lua
@@ -8,7 +8,7 @@ vim.g.NERDSpaceDelims = 1
 vim.g.NERDCompactSexyComs = 1
 
 -- Align line-wise comment delimiters flush left instead of following code indentation
-vim.g.NERDDefaultAlign = 'left'
+vim.g.NERDDefaultAlign = "left"
 
 -- Allow commenting and inverting empty lines (useful when commenting a region)
 vim.g.NERDCommentEmptyLines = 1
@@ -31,17 +31,3 @@ vim.api.nvim_set_keymap("n", "gcs", ":call nerdcommenter#Comment(0, 'Sexy')<CR>"
 vim.api.nvim_set_keymap("n", "gcy", ":call nerdcommenter#Comment(0, 'Yank')<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "gcb", ":call nerdcommenter#Comment(0, 'AlignBoth')<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "gcl", ":call nerdcommenter#Comment(0, 'AlignLeft')<CR>", { silent = true })
-
-
--- vim.api.nvim_set_keymap("v", "gcc", ":call nerdcommenter#Comment(0, 'Comment')<CR>", { silent = true })
--- vim.api.nvim_set_keymap("v", "gcu", ":call nerdcommenter#Comment(0, 'Uncomment')<CR>", { silent = true })
--- vim.api.nvim_set_keymap("v", "gc<Space>", ":call nerdcommenter#Comment(0, 'Uncomment')<CR>", { silent = true })
-
-vim.api.nvim_set_keymap("v", "gcc", ":call nerdcommenter#Comment(0, 'Toggle')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gci", ":call nerdcommenter#Comment(0, 'Invert')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gcm", ":call nerdcommenter#Comment(0, 'Minimal')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gcn", ":call nerdcommenter#Comment(0, 'Nested')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gcs", ":call nerdcommenter#Comment(0, 'Sexy')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gcy", ":call nerdcommenter#Comment(0, 'Yank')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gcb", ":call nerdcommenter#Comment(0, 'AlignBoth')<CR>", { silent = true })
-vim.api.nvim_set_keymap("v", "gcl", ":call nerdcommenter#Comment(0, 'AlignLeft')<CR>", { silent = true })
