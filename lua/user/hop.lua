02e618331c8fc279c78e850f129bb1e57d87aa83
Pui-Ching Chau <grafixen@gmail.com>
2022-08-23 22:42:04 +0800

    Additional Keybindings

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -14,3 +14,29 @@ vim.cmd("hi HopNextKey2 guifg=#ff9900")
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "s", ":HopWord<CR>", opts)
 vim.api.nvim_set_keymap("n", "S", ":HopChar1<CR>", opts)
+
+-- place this in one of your configuration file(s)
+vim.api.nvim_set_keymap(
+	"",
+	"f",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+	{}
+)
+vim.api.nvim_set_keymap(
+	"",
+	"F",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
+	{}
+)
+vim.api.nvim_set_keymap(
+	"",
+	"t",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
+	{}
+)
+vim.api.nvim_set_keymap(
+	"",
+	"T",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
+	{}
+)
