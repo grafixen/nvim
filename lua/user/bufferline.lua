94ec53fd77da9a81a6a565948bb56757d4b7b293
Pui-Ching Chau <grafixen@gmail.com>
2023-03-16 08:28:59 +0800

    [UPD] Bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -3,6 +3,10 @@ if not status_ok then
 	return
 end
 
+local function is_ft(b, ft)
+	return vim.bo[b].filetype == ft
+end
+
 bufferline.setup({
 	options = {
 		mode = "buffers",
@@ -14,25 +18,64 @@ bufferline.setup({
 		diagnostics_indicator = function(count, level, diagnostics_dict, context)
 			return "(" .. count .. ")"
 		end,
-		separator_style = "thin",
+		separator_style = "slant",
 		show_tab_indicators = true,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
 		color_icons = true,
-		custom_filter = function(buf_number, buf_numbers)
-			local tab_num = 0
-			for _ in pairs(vim.api.nvim_list_tabpages()) do
-				tab_num = tab_num + 1
-			end
-
-			if tab_num > 1 then
-				if not not vim.api.nvim_buf_get_name(buf_number):find(vim.fn.getcwd(), 0, true) then
-					return true
-				end
-			else
+		custom_filter = function(buf, buf_nums)
+			local logs = vim.tbl_filter(function(b)
+				return is_ft(b, "log")
+			end, buf_nums)
+			if vim.tbl_isempty(logs) then
 				return true
 			end
+			local tab_num = vim.fn.tabpagenr()
+			local last_tab = vim.fn.tabpagenr("$")
+			local is_log = is_ft(buf, "log")
+			if last_tab == 1 then
+				return true
+			end
+			-- only show log buffers in secondary tabs
+			return (tab_num == last_tab and is_log) or (tab_num ~= last_tab and not is_log)
 		end,
+		offsets = {
+			{
+				filetype = "undotree",
+				text = "Undotree",
+				highlight = "PanelHeading",
+				padding = 1,
+			},
+			{
+				filetype = "NvimTree",
+				text = "Explorer",
+				highlight = "PanelHeading",
+				padding = 1,
+			},
+			{
+				filetype = "DiffviewFiles",
+				text = "Diff View",
+				highlight = "PanelHeading",
+				padding = 1,
+			},
+			{
+				filetype = "flutterToolsOutline",
+				text = "Flutter Outline",
+				highlight = "PanelHeading",
+			},
+			{
+				filetype = "packer",
+				text = "Packer",
+				highlight = "PanelHeading",
+				padding = 1,
+			},
+		},
+		hover = {
+			enabled = true, -- requires nvim 0.8+
+			delay = 200,
+			reveal = { "close" },
+		},
+		sort_by = "id",
 		-- sort_by = function(buffer_a, buffer_b)
 		-- 	local mod_a = ((vim.loop.fs_stat(buffer_a.path) or {}).mtime or {}).sec or 0
 		-- 	local mod_b = ((vim.loop.fs_stat(buffer_b.path) or {}).mtime or {}).sec or 0
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -30,8 +30,13 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
+keymap("n", "<C-]>", ":BufferLineMoveNext<CR>", opts)
+keymap("n", "<C-[>", ":BufferLineMovePrev<CR>", opts)
+keymap("n", "<C-b>", ":BufferLinePick<CR>", opts)
 
 -- Insert --
 -- Daily Workflow
@@ -44,8 +49,8 @@ keymap("i", "j:", "<Esc>O", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+-- keymap("v", "<", "<gv", opts)
+-- keymap("v", ">", ">gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
