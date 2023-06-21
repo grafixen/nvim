6e5d6ea22ead9ff8da503819c82a7f6ee3a39931
Pui-Ching Chau <grafixen@gmail.com>
2023-06-21 11:40:01 +0800

    [FTR] Fidget Update & Tokyo Theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,7 +56,10 @@ return packer.startup(function(use)
 	use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
 	use("folke/which-key.nvim")
 	use("nguyenvukhang/nvim-toggler")
-	use("j-hui/fidget.nvim")
+	use({
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+	})
 
 	-- Additional Utlities (Grafixen)
 	use({
@@ -98,6 +101,7 @@ return packer.startup(function(use)
 	use("lunarvim/darkplus.nvim")
 	use("marko-cerovac/material.nvim")
 	use("ray-x/starry.nvim")
+	use("folke/tokyonight.nvim")
 
 	-- CMP
 	use("hrsh7th/nvim-cmp") -- The completion plugin
